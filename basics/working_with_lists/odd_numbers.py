957d65fdc5833cddb56b5238d1ad51cff0b5bac1
Abdulquadri Oriade <oriadeabdul17@gmail.com>
2025-01-24 23:22:20 +0100

    Completed Chapter 4 (Working with lists)

--- basics/working_with_lists/odd_numbers.py
+++ basics/working_with_lists/odd_numbers.py
@@ -1,6 +1,6 @@
 print("Odd Numbers")
 
-odd_numbers_between_1_and_20 = range(1, (20+1), 2)
+odd_numbers_from_1_to_20 = range(1, (20+1), 2)
 
-for number in odd_numbers_between_1_and_20:
+for number in odd_numbers_from_1_to_20:
     print(f"• {number}")
